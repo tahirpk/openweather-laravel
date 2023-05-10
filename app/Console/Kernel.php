ee1c08d0f194b8649afabfe30ff78a8402741450
Tahir <tahir@shopiniworld.com>
2023-05-10 15:12:46 +0400

    schulder added for everytenmints weather add

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // $schedule->command('inspire')->hourly(); withoutOverlapping(10)
+       // $schedule->command('weather:city')->everyMinute(); everyTenMinutes
+        $schedule->command('weather:all-city')->everyTenMinutes();
     }
 
     /**
